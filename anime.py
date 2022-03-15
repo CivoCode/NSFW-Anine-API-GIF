bcd937d60755cb641f5e0baceb615fa3973093bb
CivoCode <101478730+CivoCode@users.noreply.github.com>
2022-03-15 21:07:10 +0000

    Create anime.py

---
+++ anime.py
@@ -0,0 +1,40 @@
+
+import discord
+import requests
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.command()
+@commands.is_nsfw()
+async def hentai(ctx):
+    r = requests.get("https://nekos.life/api/v2/img/hentai")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+@commands.is_nsfw()
+async def boobs(ctx):
+    r = requests.get("https://nekos.life/api/v2/img/boobs")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@bot.command()
+@commands.is_nsfw()
+async def bj(ctx):
+    r = requests.get("https://nekos.life/api/v2/img/bj")
+    res = r.json()
+    em = discord.Embed()
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+
+# https://nekos.life/api/v2/endpoints
+
+bot.run("ODkyNTA0MTEwNzA2ODUxODYw.YVN3OQ.ncdVQ7ZCcaV67zhYIFqUsiXPvSo")
